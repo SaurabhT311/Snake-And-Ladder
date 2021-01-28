aa29341905833f0e84e9bfc8d07748269a5d047b
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-01-28 23:01:41 +0530

    [Saurabh]Add Initialised player position at 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,1 +1,8 @@
-system.out.println("Welcome")
+public class SnakeLadder
+{
+	public static void main(String[] args)
+	{
+		int position=0;
+		System.out.println("Game starting in single player mode");
+	}
+}
