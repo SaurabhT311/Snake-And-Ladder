34732297b2ee2f1eed998189a3b7179bf5f07fb6
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2021-01-29 21:00:26 +0530

    [Saurabh]Added RollingTheDie

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,6 @@ public class SnakeLadder
 		int position=0;
 		System.out.println("Game starting in single player mode");
 		int dieValue=(int)(Math.floor(Math.random()*10 % 6) + 1);
-		System.out.println(dieValue);
+		System.out.println("Die value is :" +dieValue);
 	}
 }
