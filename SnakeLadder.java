1c003bdd69eb235c25e8183a44de738259b14106
SaurabhT311 <tripathisaurabh.311@gmail.com>
2021-01-29 23:13:06 +0530

    [Saurabh]Added PlayingGameTillPositionReachesTo100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,10 +1,40 @@
 public class SnakeLadder
 {
-	public static void main(String[] args)
-	{
-		int position=0;
-		System.out.println("Game starting in single player mode");
-		int dieValue=(int)(Math.floor(Math.random()*10 % 6) + 1);
-		System.out.println(dieValue);
-	}
+        static final int NO_PLAY=0;
+        static final int LADDER=1;
+        static final int SNAKE=2;
+
+        public static void main(String[] args)
+        {
+                int position=0;
+                System.out.println("Game starting in single player mode");
+                while(position!=100)
+		{
+			int dieValue=(int)(Math.floor(Math.random()*10 % 6) + 1);
+                	int choice=(int)Math.floor(Math.random()*10 % 3);
+                	switch(choice)
+                	{
+                        	case NO_PLAY:
+                                	System.out.println("Player did not move: "+ position);
+                                	break;
+                        	case LADDER:
+                                	position=position+dieValue;
+                                	System.out.println("Player position after climbing ladder: "+ position);
+                                	break;
+                        	case SNAKE:
+                                	position=position-dieValue;
+					if(position<0)
+					{
+						position=0;
+					}
+                                	System.out.println("Player bit by snake: "+ position);
+                               		 break;
+                        	default:
+                                	System.out.println("Choose valid option");
+                                	break;
+               		 }
+                }
+        }
 }
+
+
